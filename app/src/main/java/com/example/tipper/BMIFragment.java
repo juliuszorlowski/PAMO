888968d83e36db50a34e4c4126d4e8d77be902c2
Juliusz <orlowski.juliusz@gmail.com>
2022-04-02 20:37:59 +0200

    Added calories and recipes

--- app/src/main/java/com/example/tipper/BMIFragment.java
+++ app/src/main/java/com/example/tipper/BMIFragment.java
@@ -59,6 +59,6 @@ public class BMIFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_bmi, container, false);
     }
 }
